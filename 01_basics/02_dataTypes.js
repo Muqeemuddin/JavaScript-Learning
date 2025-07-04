84892eb4e1168738686ed23167c6922c2151b652
Muqeemuddin Mohammed <126502443+Muqeemuddin@users.noreply.github.com>
2025-07-04 21:30:24 +0000

    learned datatypes

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -1,1 +1,32 @@
 "use strict" // treat all JS code as newer version.
+
+/*
+    For referring the documentation of a JavaScript:
+
+    mdn - an unofficial documentation provided by mozilla.
+
+    https://tc39.es/ecma262/ - an official documentation provided by the ECMA Internation.
+
+    ECMAscript defines the standards for JavaScript.
+
+
+*/
+
+// Primitive datatype
+
+let name = "Muqeem" // String. In JavaScript you can enclosed the string with single or double quotes.
+let age = 21 // int or number
+let isLoggedIn = false //boolean
+
+//bigint
+let temp = null // standalone value is a object datatype
+let mobileNo; // undefined
+//symbol - is used t define a unique componenets.
+
+console.log(typeof mobileNo);
+console.log(typeof(temp))
+
+// Object datatype.
+
+
+

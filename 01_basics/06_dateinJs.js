0efa78f4718fb5c12a731552385bafc546af006b
Muqeemuddin Mohammed <126502443+Muqeemuddin@users.noreply.github.com>
2025-07-05 22:23:43 +0000

    learned about Date Object

---
+++ 01_basics/06_dateinJs.js
@@ -0,0 +1,13 @@
+let currentDate = new Date();
+
+console.log(currentDate);
+console.log(currentDate.toString());
+console.log(currentDate.toDateString());
+console.log(currentDate.toISOString());
+console.log(currentDate.toJSON());
+console.log(currentDate.toLocaleDateString('en-IN'));
+
+// datetype of a currentDate is Object.
+
+
+

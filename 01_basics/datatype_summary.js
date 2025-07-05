b691989c6c61d01b8bf8a76e1b73c368b59a7c75
Muqeemuddin Mohammed <126502443+Muqeemuddin@users.noreply.github.com>
2025-07-05 13:42:54 +0000

    Memory management learned for datatypes.

--- 01_basics/datatype_summary.js
+++ 01_basics/datatype_summary.js
@@ -32,3 +32,16 @@ console.log(typeof lastTenMatchesScore)
 
 // article by ecma-international for referring the type of datatypes:
 // https://262.ecma-international.org/5.1/#sec-11.4.3
+
+// Memory Management:
+
+// In JavaScript, similar to Java, The varaibles are stored in two types of memory.
+// 1. Stack and 2. Heap
+
+// Primitive variables are stored in the Stack memory and Non-Primitive variables are stored in the heap memory.
+
+// When copying the primitive variables, their actual value gets copied. Hence when changes in original or copied variable doesn't
+// reflect the change in copied or original variable. 
+// Where as, in the refernce variables(Non-primitive), as the name suggests, copied varibles store the refernce of the heap memory
+// where the actual value is stored. Hence, changes in original/copied variables reflect the change in copied/original variables too.
+

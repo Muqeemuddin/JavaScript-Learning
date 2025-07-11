5a17e6005ac8a269ea831f0b708a9ef01e518c36
Muqeemuddin Mohammed <126502443+Muqeemuddin@users.noreply.github.com>
2025-07-11 15:19:13 +0000

    changes added to BMI

--- 06_projects/02-BMICalculator/bmiCalculator.js
+++ 06_projects/02-BMICalculator/bmiCalculator.js
@@ -1,11 +1,17 @@
-document.querySelector('button').addEventListener('click', (e) =>{
+const form = document.querySelector('form');
+// event type is not click but a submit. Here the button is clicked for submitting a form. So event Type should be taken as submit.
+form.addEventListener('submit', (e) =>{
+    e.preventDefault(); // Upon submitting the form, the immediate action will be to send the form details to the server. as the submit action involves either get or post request.
     const height = parseInt(document.querySelector('#height').value)
     const weight = parseInt(document.querySelector('#weight').value)
-    if(height === NaN || weight === NaN){
-        document.querySelector('#results').appendChild(document.createTextNode("Please enter valid numbers"))
+    const results = document.querySelector('#results');
+    if(height ==='' || height <0 || isNaN(height)){
+        results.innerHTML = "Please enter valid height"
+    }else if(weight ==='' || weight <0 || isNaN(weight)){
+        results.innerHTML = "Please enter valid weight"
     }else{
         const bmi = Math.round((weight/ ((height*height)/10000))*10) / 10;
-        document.querySelector('#results').innerHTML = `Your BMI is ${bmi}`;
+        results.innerHTML = `Your BMI is ${bmi}`;
     }
 })
 
